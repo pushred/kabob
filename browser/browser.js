c29a6fff2f8ea7f7edf45ad75846bdd79737341b
Eric Lanehart <pushred@gmail.com>
2016-08-25 21:59:13 -0400

    Persist state in localStorage

--- browser/browser.js
+++ browser/browser.js
@@ -1,17 +1,26 @@
 const App = require('@app/universal/containers/app.jsx');
 const React = require('react');
 const redux = require('redux');
-const reducers = require('@app/universal/state/reducers');
+const rootReducer = require('@app/universal/state/reducers');
 const render = require('react-dom').render;
+
+const persistState = require('redux-localstorage').default;
+const adapter = require('redux-localstorage/lib/adapters/localStorage');
+const mergePersistedState = require('redux-localstorage').mergePersistedState;
 
 function init () {
   const initialState = window.__STATE__;
 
-  const createStore = redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )(redux.createStore);
+  const reducer = redux.compose(
+    mergePersistedState()
+  )(rootReducer);
 
-  const store = createStore(reducers, initialState);
+  const enhancer = redux.compose(
+    persistState(adapter(window.localStorage), 'kabob-combos'),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
+
+  const store = redux.createStore(reducer, initialState, enhancer);
 
   render(
     <App store={store} />,
